745ce3163320495bef3617504b59f830a73fc767
Rav53 <Rav5385@gmail.com>
2022-09-28 17:56:49 +0300

    ДЗ Задачи второго семинара

--- Seminar 2 Zadacha3.py
+++ Seminar 2 Zadacha3.py
@@ -3,8 +3,7 @@
 
 n = int(input('Введите число: ')) 
 
-def sequence(n):
+def list(n):
     return[round((1 + 1 / n)**n, 2)for n in range (1, n + 1)]   
-        
-print(sequence(n))
-print(round(sum(sequence(n))))
+#print(list(n))
+print(round(sum(list(n))))
